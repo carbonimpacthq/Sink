f4055abf672e5fdc54ad73b167d263036d8dbb59
Carbon Impact <webmaster@carbonimpacthq.com>
2025-05-24 00:51:21 +0700

    Update nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -24,7 +24,7 @@ export default defineNuxtConfig({
     redirectStatusCode: '301',
     linkCacheTtl: 60,
     redirectWithQuery: false,
-    homeURL: '',
+    homeURL: 'https://go.carbonimpacthq.com',
     cfAccountId: '',
     cfApiToken: '',
     dataset: 'sink',
